a8d4dd4effa55d966c1f177e2608758bdc9a7daa
Guilherme Pereira <guilherme.h12@gmail.com>
2019-07-20 21:25:41 -0300

    add single clfs

---
+++ single-clfs.py
@@ -0,0 +1,79 @@
+import csv
+
+from nltk import TweetTokenizer
+from nltk.corpus import stopwords
+from sklearn import model_selection, metrics
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from sklearn.linear_model import PassiveAggressiveClassifier, LogisticRegression
+from sklearn.svm import SVC
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import VotingClassifier, AdaBoostClassifier, BaggingClassifier
+from sklearn.model_selection import KFold
+
+
+def load(csv_file):
+    polaridade = list()
+    opinioes = list()
+    with open(csv_file, 'r', encoding='utf-8') as csv_file:
+        try:
+            reader = csv.reader(csv_file, delimiter='|')
+            for row in reader:
+
+                polaridade.append(int(str(row[0]).replace('\ufeff', '')))
+                texto = row[2].lower()
+                texto = TweetTokenizer().tokenize(texto)
+                # for t in texto:
+                #     if 'https' in t:
+                #         texto.remove(t)
+                #     if '#' in t:
+                #         texto.remove(t)
+                #     if '@' in t:
+                #         texto.remove(t)
+
+                opinioes.append(texto)
+
+        except IOError:
+            pass
+    return opinioes, polaridade
+
+
+def pre():
+    csv_file = 'corpus-politica.csv'
+    x, y = load(csv_file)
+    vectorizer = TfidfVectorizer()
+    #vectorizer = TfidfVectorizer(ngram_range=(2, 2), analyzer='word')
+    pt_br_stop_words = set(stopwords.words('portuguese'))
+
+    for i in range(len(x)):
+        # x[i] = [w for w in x[i] if not w in pt_br_stop_words]
+        x[i] = ' '.join(x[i])
+    x = vectorizer.fit_transform(x)
+
+    return x, y
+
+
+def pro():
+    x, y = pre()
+    kfold = KFold(n_splits=10)
+
+    clfs = [
+        SVC(),
+        MultinomialNB(),
+        LogisticRegression(),
+        PassiveAggressiveClassifier()
+    ]
+
+    for c in clfs:
+        result = model_selection.cross_val_predict(c, x, y, cv=kfold)
+        print("%s Result" % str(c).split("(")[0])
+        print("Acuracia: %f" % metrics.accuracy_score(y, result))
+        print("Precisaão: %f" % metrics.precision_score(y, result, average='weighted'))
+        print("F-score: %f" % metrics.f1_score(y, result, average='weighted'))
+        print("Recall: %f" % metrics.recall_score(y, result, average='weighted'))
+        print(metrics.confusion_matrix(y, result, labels=(1, 2, 3)))
+
+
+if __name__ == '__main__':
+    pro()
+
